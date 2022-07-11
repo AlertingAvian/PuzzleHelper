5714d9d501d23bb6bb2b2ad43eb3a5d474c4d2b3
Patrick Maloney <boyscoutpatrick813@gmail.com>
2022-07-11 01:20:04 -0500

    Add project files.

---
+++ PuzzleBlock.cs
@@ -0,0 +1,21 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+
+namespace Celeste.Mod.PuzzleHelper
+{
+    [CustomEntity("PuzzleHelper/PuzzleFallingBlock", "PuzzleHelper/PuzzleBlock")]
+    public class PuzzleBlock : FallingBlock
+    {
+        public PuzzleBlock(Vector2 position, char tile, int width, int height, bool finalBoss, bool behind, bool climbFall) : base(position, tile, width, height, finalBoss, behind, climbFall)
+        {
+
+        }
+
+        public PuzzleBlock(EntityData data, Vector2 offset) : this(data.Position + offset, 'a', 1, 1, false, false, false) // not set up properly
+        {
+
+        }
+    }
+}
---
+++ PuzzleHelper.cs
@@ -0,0 +1,24 @@
+﻿using Monocle;
+
+namespace Celeste.Mod.PuzzleHelper
+{
+    public class PuzzleHelper : EverestModule
+    {
+        public static PuzzleHelper Instance;
+
+        public PuzzleHelper()
+        {
+            Instance = this;
+        }
+
+        public override void Load()
+        {
+            Logger.SetLogLevel("PuzzleHelper", LogLevel.Verbose);
+            Logger.Log(LogLevel.Debug, "PuzzleHelper", "Loaded.");
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
